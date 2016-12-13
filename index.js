023cffc9aa296aaff7830039267baa178e367b20
dagurleo <dagurleo@gmail.com>
2016-12-13 21:31:20 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// const printer = require('printer'),
+const fs = require('fs'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    printer = require('printer');
+
+var lastPrinted;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+const port = 4730;
+app.listen(port, () => {
+    console.log("Print server started on port: " + port);
+});
+
+
+
+app.post('/api/print', (req, res) => {
+    if(lastPrinted != req.body.printNumber){
+        if(printer.getDefaultPrinterName() == 'THERMAL'){
+            // console.log(req.body.buffer);
+            // var i = fs.readFileSync(new Buffer(req.body.buffer, 'base64'));
+            var smegma = new Buffer(req.body.buffer, 'base64');
+            printer.printDirect({
+                data: smegma,
+                type: 'PDF',
+                success: function(jobID){
+                    lastPrinted = req.body.printNumber;
+                    res.status(201).send('Printed successfully');
+                },
+                error: function(err){
+                    res.status(500).send(err);
+                }
+            });
+
+
+        }
+    }
+});
